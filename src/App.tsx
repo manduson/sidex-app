7c0fbe1ee8681acc1a8f1682297d1671b6077537
manduson <manduson@gmail.com>
2026-05-06 21:22:47 +0900

    Fix typescript unused import errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Feed from './pages/Feed';
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabase';
 import { Check, CheckCircle2, Clock } from 'lucide-react';
 
